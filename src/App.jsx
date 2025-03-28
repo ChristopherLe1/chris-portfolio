e15e69760e5cff0b52b3a4a46f1dcb5c5bf6e87b
ChristopherLe1 <christopher.le@aa.com>
2025-03-28 10:49:33 -0500

    feat: add new SVG icons and enhance profile layout with contact information

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { CiLocationOn, CiMail, CiPhone } from "react-icons/ci";
+import { FaGithub, FaLinkedin, FaBlog } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -12,17 +10,41 @@ function App() {
         <div className="about-section">
           <div className="profile">
             <div className="profile-pic">
-              <img src="src/assets/profile-pic.JPG" alt="Profile picture" />
+              <img src="../src/assets/profile-pic.jpg" alt="Profile picture" />
             </div>
             <div className="name">Chris Le</div>
-            <div className="job-title">Frontend Developer</div>
+            <div className="job-title">Frontend Developer | JavaScript Developer | Project Engineer</div>
           </div>
-          <div className="info">Information</div>
+          <div className="info">
+            <div className="location">
+              <CiLocationOn size={24}/>
+              <span>Dallas, TX</span>
+            </div>
+            <div className="email">
+              <CiMail size={24}/>
+              <a href="mailto:ChristopherAnhDLe@gmail.com">ChristopherAnhDLe@gmail.com</a>
+            </div>
+            <div className="phone">
+              <CiPhone size={24}/>
+              <a href="tel:972-804-2797">(972) 804-2797</a>
+            </div>
+            <div className="social-icons">
+              <a href="http://www.linkedin.com/in/christopherle2"><FaLinkedin size={24} />
+              </a>
+              <a href="http://www.linkedin.com/in/christopherle2"><FaGithub size={24} />
+              </a>
+              <a href="https://cdlephotos.com/"><FaBlog size={24}/></a>
+            </div>
+          </div>
           <div className="skills">Tech Stack</div>
           <div className="education">Education</div>
         </div>
         <div className="projects-section">
-          <div className="projects">Projects</div>
+          <div className="projects">
+            <div className="project-one"></div>
+            <div className="project-two"></div>
+            <div className="project-three"></div>
+          </div>
           <div className="experience">Experience History</div>
         </div>
       </div>
